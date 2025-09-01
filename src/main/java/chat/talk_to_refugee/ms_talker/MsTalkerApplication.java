eaabdba4d99b806a31d8c5d0fe84046a598efc66
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-01 14:39:31 -0300

    feat: project base generated by initializr

---
+++ src/main/java/chat/talk_to_refugee/ms_talker/MsTalkerApplication.java
@@ -0,0 +1,13 @@
+package chat.talk_to_refugee.ms_talker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTalkerApplication.class, args);
+	}
+
+}
