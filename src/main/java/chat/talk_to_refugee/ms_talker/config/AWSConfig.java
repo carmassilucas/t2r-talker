b98659e483805b1f82426eadc233e0e566b7ae48
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-08 11:44:37 -0300

    feat: configuring aws s3 client

---
+++ src/main/java/chat/talk_to_refugee/ms_talker/config/AWSConfig.java
@@ -0,0 +1,39 @@
+package chat.talk_to_refugee.ms_talker.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class AWSConfig {
+
+    private final String endpoint;
+    private final String accessKey;
+    private final String secretKey;
+
+    public AWSConfig(@Value("${aws.localstack.endpoint}") String endpoint,
+                     @Value("${aws.localstack.credentials.access-key}") String accessKey,
+                     @Value("${aws.localstack.credentials.secret-key}") String secretKey) {
+        this.endpoint = endpoint;
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .forcePathStyle(true)
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)
+                ))
+                .build();
+    }
+}
