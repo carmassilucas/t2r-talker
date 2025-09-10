7e837425ff1e759cf401fb2fbca039edebdbd028
lucas.carmassi <lucas.carmassi@gruporevise.com.br>
2025-09-10 13:28:47 -0300

    feat: facade in toggle active profile use case

--- src/main/java/chat/talk_to_refugee/ms_talker/usecase/ToggleActiveProfileUseCase.java
+++ src/main/java/chat/talk_to_refugee/ms_talker/usecase/ToggleActiveProfileUseCase.java
@@ -1,7 +1,7 @@
 package chat.talk_to_refugee.ms_talker.usecase;
 
 import chat.talk_to_refugee.ms_talker.exception.TalkerNotFoundException;
-import chat.talk_to_refugee.ms_talker.repository.TalkerRepository;
+import chat.talk_to_refugee.ms_talker.usecase.facade.ToggleActiveProfileFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,24 +12,24 @@ import java.util.UUID;
 public class ToggleActiveProfileUseCase {
 
     private static final Logger log = LoggerFactory.getLogger(ToggleActiveProfileUseCase.class);
-    private final TalkerRepository repository;
 
-    public ToggleActiveProfileUseCase(TalkerRepository repository) {
-        this.repository = repository;
+    private final ToggleActiveProfileFacade dependencies;
+
+    public ToggleActiveProfileUseCase(ToggleActiveProfileFacade dependencies) {
+        this.dependencies = dependencies;
     }
 
     public void execute(UUID id, Boolean activate) {
         log.info("{} talker profile", activate ? "Activating" : "Disabling");
 
-        var talker = repository.findById(id)
+        var talker = this.dependencies.repository().findById(id)
                 .orElseThrow(TalkerNotFoundException::new);
 
         if (!talker.getActive().equals(activate)) {
             talker.setActive(activate);
-            this.repository.save(talker);
+            this.dependencies.repository().save(talker);
 
             log.info("Talker status update done");
-
             return;
         }
 
